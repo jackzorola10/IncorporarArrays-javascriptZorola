1af68dc28798049425f459e059cdd692aa7b8d7e
Jack Zorolasl <jackzorola10@gmail.com>
2022-04-22 00:12:18 -0500

    added constructor

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 
-
-
-
 const ropa = [
     { id:1, color: "Azul", tipo: "vestido", talla: "Mediano" },
     { id:2, color: "Rojo", tipo: "Pantalones", talla: "Pequeño" },
@@ -16,19 +13,28 @@ for (const item of ropa ) {
     const newArray = [ item.id, item.color, item.tipo, item.talla ];
 
     menu.push("Prenda " + item.id + " - " + "color: " + item.color + ", de tipo: " + item.tipo + ", y talla: " + item.talla + ".");
-
 } 
 
 let printedMenu = menu.join("\n");
 console.log(printedMenu)
 
+class cloth {
+    constructor(id, color, tipo, talla) {
+        this.id = ropa.length + 1;
+        this.color = colorInput;
+        this.tipo = tipoInput;
+        this.talla = tallaInput;
+    }
+}
 
 alert("Bienvenido al intercambio de ropa de segunda mano, donde puede traer una prenda que ya no quieras y cambiarla por otra.") 
 
-prompt(
+let userSelection = prompt(
     "De momento estas son las prendas que tenemos disponbiles, por favor selecciona incluyendo el numero de la prenda que vos queres:\n\n" +
     printedMenu
 ); 
 
+let tipoInput = prompt("Por favor ingresa el tipo de prenda que entregaras, por ejemplo: 'Camiseta'");
+let colorInput = prompt("Por favor ingresa el color de prenda");
 
 
